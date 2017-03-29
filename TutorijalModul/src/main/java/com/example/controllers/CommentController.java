0edb8c725ff639750fac29124e80fa52d138b409
mathildaa <iivkovic2@etf.unsa.ba>
2017-03-29 09:47:52 +0200

    kontroleri ispravka,

--- TutorijalModul/src/main/java/com/example/controllers/CommentController.java
+++ TutorijalModul/src/main/java/com/example/controllers/CommentController.java
@@ -4,9 +4,11 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.models.Comment;
 
+@RestController
 public class CommentController {
 
 	
