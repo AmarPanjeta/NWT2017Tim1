d96796f00fd3c78855575e8c499b67505b05d0a5
mathildaa <iivkovic2@etf.unsa.ba>
2017-03-29 08:08:31 +0200

    configuracije i modul v1

--- TutorijalModul/src/main/java/com/example/repositories/CommentRepository.java
+++ TutorijalModul/src/main/java/com/example/repositories/CommentRepository.java
@@ -1,11 +1,18 @@
 package com.example.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.models.Comment;
+import com.example.models.Tag;
 
 @RepositoryRestResource(path="comments", collectionResourceRel="comments")
 public interface CommentRepository extends CrudRepository<Comment, Long>{
 
+//public List<Comment> findFirst10ByName(String name);
+	
 }
+
+
