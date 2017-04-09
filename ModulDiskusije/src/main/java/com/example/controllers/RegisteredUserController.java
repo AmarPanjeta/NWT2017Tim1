7ce5e6ed93081748ff5a28ee9472e1fdb14fedc5
LejlaNukic <lnukic2@etf.unsa.ba>
2017-04-09 16:07:21 +0200

    Discussions-Users communication added

--- ModulDiskusije/src/main/java/com/example/controllers/RegisteredUserController.java
+++ ModulDiskusije/src/main/java/com/example/controllers/RegisteredUserController.java
@@ -2,9 +2,11 @@ package com.example.controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.models.RegisteredUser;
 
@@ -12,6 +14,9 @@ import com.example.models.RegisteredUser;
 public class RegisteredUserController {
 	
 	private final AtomicLong br = new AtomicLong();
+	
+	@Autowired
+	RestTemplate restTemplate;
 	
 	@RequestMapping("/korisnici")
 	public RegisteredUser korisnici(@RequestParam(value="username") String username){
@@ -21,6 +26,14 @@ public class RegisteredUserController {
 		rg.setId(br.incrementAndGet());
 		return rg;
 		
+	}
+	
+	@RequestMapping("/isadmin")
+	
+	public String isAdmin(@RequestParam(value="username") String username){
+		
+		String regUser=this.restTemplate.getForObject("http://users-client/users", String.class);
+		return regUser;
 	}
 
 }
