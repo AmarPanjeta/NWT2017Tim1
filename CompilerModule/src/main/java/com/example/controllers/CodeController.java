4601d1161b4d140763b533f688a29f73cdcac1df
AmarPanjeta <amar.panjeta@gmail.com>
2017-04-13 15:41:53 +0200

    Codecontroller - added method for running codes with tests

--- CompilerModule/src/main/java/com/example/controllers/CodeController.java
+++ CompilerModule/src/main/java/com/example/controllers/CodeController.java
@@ -1,7 +1,10 @@
 package com.example.controllers;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 import javax.servlet.ServletException;
@@ -44,6 +47,49 @@ public class CodeController {
 			String output=s.hasNext() ? s.next(): "nema";
 			System.out.println(output);
 			result.result=output;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			result.status="not working";
+		}
+		return result;
+	}
+	
+	@RequestMapping("runwithat")
+	public ResultClass runWithATs(@RequestBody CodeClass code){
+		System.out.println(code.code);
+		System.out.println(code.username);
+		ResultClass result=new ResultClass("working", "/");
+		Runtime r=Runtime.getRuntime();
+		ProcessBuilder pb=new ProcessBuilder("printf",code.code).redirectOutput(new File("prog.cpp"));
+		try {
+			Process p = r.exec("rm prog.cpp");
+			p.waitFor();
+			pb.start().waitFor();
+			pb=new ProcessBuilder("g++","-o","prog","prog.cpp");
+			pb.start().waitFor();
+			
+			int numberOfPassing=0;
+			for(int i=0;i<code.tests.length;i+=2){
+				p=r.exec("./prog");
+				InputStream is = p.getInputStream();
+				OutputStream os=p.getOutputStream();
+				Scanner s= new Scanner(is).useDelimiter("\\A");
+				BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(os));
+				writer.write(code.tests[i]+"\n\n");
+				writer.flush();
+				p.waitFor();
+				System.out.println(code.tests[i]);
+				//writer.write("\n");
+				String output=s.hasNext() ? s.next(): "nema";
+				System.out.println(output+"\n");
+				if(code.tests[i+1].equals(output))numberOfPassing++;
+			}
+			
+			
+			//System.out.println(output);
+			result.result=Integer.toString(numberOfPassing);
+			result.status="compiles";
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
