bc2491bc2579b3cf21f8716a8068eac5e17afd4c
LejlaNukic <lnukic2@etf.unsa.ba>
2017-06-11 12:02:14 +0200

    disc

--- react_app/src/components/discussions/Discussion.js
+++ react_app/src/components/discussions/Discussion.js
@@ -1,17 +1,60 @@
 import React, {Component} from "react";
+import { Button, Card, Row, Col, Collection,CollectionItem,Icon } from 'react-materialize';
+
+
+var rest, mime, client;
+
+rest = require('rest');
+mime = require('rest/interceptor/mime');
+client = rest.wrap(mime);
 
 export class Discussion extends Component{
 
 	constructor(props){
 		super(props);
+		this.state={user:{username:'',email:''}};
 	}
+    
 
+    componentDidMount(){
+    	client({
+    		method:'GET',
+    		path:'http://localhost:8082/discussions/'+this.props.discussion.id+'/regUser'
+    	}).then(response=>{
+    		console.log("odgovor",response);
+    	this.setState({user:response.entity})
+    		}
+    	)
+    }
 	render(){
+		console.log(this.props.discussion);
 		return(
-			<tr>
-				<td>{this.props.discussion.title}</td>
-				<td>{this.props.discussion.text}</td>
-			</tr>
+
+
+
+           <CollectionItem>
+			<Col m={6} s={12}>
+			    <Icon>account_circle</Icon>
+			    <span><b>{this.state.user.username}</b> |
+			       	{this.props.discussion.open &&
+					<span><b>Otvorena</b></span>
+						}
+
+					{this.props.discussion.open==false && 
+							<span><b>Zatvorena</b></span>
+						}
+			    </span>
+				<Card className='blue-grey darken-1' textClassName='white-text' title={this.props.discussion.title} actions={[<a href='#'>Prikazi detalje</a>]}>
+				{this.props.discussion.text}
+				
+				<Icon right>favorite</Icon>
+				
+			    <Icon right>opacity</Icon>
+				
+				</Card>
+			</Col>
+
+			</CollectionItem>
 		)
 	}
 }
